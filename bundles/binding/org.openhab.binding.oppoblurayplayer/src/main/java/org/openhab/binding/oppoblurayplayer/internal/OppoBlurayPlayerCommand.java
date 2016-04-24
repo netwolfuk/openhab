0d15a8e44859cdf99c7d27f852773ee0571c6aa5
netwolfuk <netwolfuk@gmail.com>
2016-04-24 20:42:40 +1200

    More converage of the Oppo API.

--- bundles/binding/org.openhab.binding.oppoblurayplayer/src/main/java/org/openhab/binding/oppoblurayplayer/internal/OppoBlurayPlayerCommand.java
+++ bundles/binding/org.openhab.binding.oppoblurayplayer/src/main/java/org/openhab/binding/oppoblurayplayer/internal/OppoBlurayPlayerCommand.java
@@ -74,7 +74,107 @@ public enum OppoBlurayPlayerCommand {
 	VERBOSE_MODE_0			(OppoBlurayPlayerCommandType.VERBOSITY, StringType.EMPTY, "QVM", "SVM 0", "OK 0", "SVM OK 0"),
 	VERBOSE_MODE_1			(OppoBlurayPlayerCommandType.VERBOSITY, StringType.EMPTY, "QVM", "SVM 1", "OK 1", "SVM OK 1"),
 	VERBOSE_MODE_2			(OppoBlurayPlayerCommandType.VERBOSITY, StringType.EMPTY, "QVM", "SVM 2", "OK 2", "SVM OK 2"),
-	VERBOSE_MODE_3			(OppoBlurayPlayerCommandType.VERBOSITY, StringType.EMPTY, "QVM", "SVM 3", "OK 3", "SVM OK 3");
+	VERBOSE_MODE_3			(OppoBlurayPlayerCommandType.VERBOSITY, StringType.EMPTY, "QVM", "SVM 3", "OK 3", "SVM OK 3"),
+	
+	INPUT_SOURCE_BD_PLAYER	(OppoBlurayPlayerCommandType.INPUT_SOURCE, StringType.EMPTY, "QIS", "SIS 0", "OK 0 BD-PLAYER", "SIS OK 0 BD-PLAYER"),
+	INPUT_SOURCE_HDMI_FRONT	(OppoBlurayPlayerCommandType.INPUT_SOURCE, StringType.EMPTY, "QIS", "SIS 1", "OK 1 HDMI-FRONT", "SIS OK 1 HDMI-FRONT"),
+	INPUT_SOURCE_HDMI_BACK	(OppoBlurayPlayerCommandType.INPUT_SOURCE, StringType.EMPTY, "QIS", "SIS 2", "OK 2 HDMI-BACK", "SIS OK 2 HDMI-BACK"),
+	INPUT_SOURCE_ARC		(OppoBlurayPlayerCommandType.INPUT_SOURCE, StringType.EMPTY, "QIS", "SIS 3", "OK 3 ARC", "SIS OK 3 ARC"),
+	INPUT_SOURCE_OPTICAL	(OppoBlurayPlayerCommandType.INPUT_SOURCE, StringType.EMPTY, "QIS", "SIS 4", "OK 4 OPTICAL", "SIS OK 4 OPTICAL"),
+	INPUT_SOURCE_COAXIAL	(OppoBlurayPlayerCommandType.INPUT_SOURCE, StringType.EMPTY, "QIS", "SIS 5", "OK 5 COAXIAL", "SIS OK 5 COAXIAL"),
+	INPUT_SOURCE_USB_AUDIO	(OppoBlurayPlayerCommandType.INPUT_SOURCE, StringType.EMPTY, "QIS", "SIS 6", "OK 6 USB-AUDIO", "SIS OK 6 USB-AUDIO"),
+	
+	// TODO: Volume level needs some more thought.
+	// Toggling mute (MUT) off will return actual volume level.
+	// Currently only 0% and 100% are catered for.
+	VOLUME_LEVEL_000		(OppoBlurayPlayerCommandType.VOLUME_LEVEL, DecimalType.ZERO,  "QVL", "SVL 0", "OK 0", "UVL OK 000"),
+	VOLUME_LEVEL_100		(OppoBlurayPlayerCommandType.VOLUME_LEVEL, DecimalType.ZERO,  "QVL", "SVL 100", "OK 100", "UVL OK 100"),
+	VOLUME_LEVEL_MUTE		(OppoBlurayPlayerCommandType.VOLUME_LEVEL, OnOffType.OFF,  "QVL", "SVL MUT", "OK MUT", "UVL OK MUT"),
+	
+	/*
+	  QDT
+	  	OK BD-MV
+		OK DVD-VIDEO
+		OK DVD-AUDIO
+		OK SACD
+		OK CDDA
+		OK HDCD
+		OK DATA-DISC
+	  
+	  
+	  UDT - Disc Type Update:
+		Sent when a new disc type is detected.
+		Possible Parameters: 4 chars
+		BDMV - Blu-ray Disc
+		DVDV – DVD-Video
+		DVDA – DVD-Audio
+		SACD
+		CDDA
+		HDCD
+		DATA – Data disc
+		VCD2 – VCD 2.0
+		SVCD - SVCD
+		Example: @UDT DVDV
+	 */
+							//	Type								State			   Qry	  Cmd	 Response	 Event
+	DISC_TYPE_BLURAY_DISC	(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK BD-MV", "UDT BDMV"),
+	DISC_TYPE_DVD_VIDEO		(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK DVD-VIDEO", "UDT DVDV"),
+	DISC_TYPE_DVD_AUDIO		(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK DVD-AUDIO", "UDT DVDA"),
+	DISC_TYPE_SACD			(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK SACD", "UDT SACD"),
+	DISC_TYPE_CDDA			(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK CDDA", "UDT CDDA"),
+	DISC_TYPE_HDCD			(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK HDCD", "UDT HDCD"),
+	DISC_TYPE_DATA_DISC		(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK DATA-DISC", "UDT DATA"),
+	DISC_TYPE_DATA_VCD2		(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK VCD2", "UDT VCD2"),
+	DISC_TYPE_DATA_SVCD		(OppoBlurayPlayerCommandType.DISC_TYPE, StringType.EMPTY,  "QDT", "???", "OK SVCD", "UDT SVCD"),
+	
+	/*
+	
+		QHD - Query HDMI resolution	
+			OK 480P
+			OK 720P50
+			OK 1080P60
+			OK AUTO
+			
+		SHD 
+			SDI
+			SDP
+			720P
+			1080I
+			1080P
+			SRC
+			AUTO
+		UHD
+			unknown if UDH updates are sent.
+			TODO: Monitor log and see.
+	*/
+	
+	HDMI_RESOLUTION_SDI_480		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD SDI", "OK 480I", "???"),
+	HDMI_RESOLUTION_SDI_576		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD SDI", "OK 576I", "???"),
+	HDMI_RESOLUTION_SDP_480		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD SDP", "OK 480P", "???"),
+	HDMI_RESOLUTION_SDP_576		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD SDP", "OK 576P", "???"),
+	
+	HDMI_RESOLUTION_720P_24		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 720P", "OK 720P25", "???"),
+	HDMI_RESOLUTION_720P_25		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 720P", "OK 720P30", "???"),
+	HDMI_RESOLUTION_720P_30		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 720P", "OK 720P30", "???"),
+	HDMI_RESOLUTION_720P_50		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 720P", "OK 720P50", "???"),
+	HDMI_RESOLUTION_720P_60		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 720P", "OK 720P60", "???"),
+	
+	HDMI_RESOLUTION_1080I_24	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080I", "OK 1080I25", "???"),
+	HDMI_RESOLUTION_1080I_25	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080I", "OK 1080I30", "???"),
+	HDMI_RESOLUTION_1080I_30	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080I", "OK 1080I30", "???"),
+	HDMI_RESOLUTION_1080I_50	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080I", "OK 1080I50", "???"),
+	HDMI_RESOLUTION_1080I_60	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080I", "OK 1080I60", "???"),
+	
+	HDMI_RESOLUTION_1080P_24	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080P", "OK 1080P25", "???"),
+	HDMI_RESOLUTION_1080P_25	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080P", "OK 1080P30", "???"),
+	HDMI_RESOLUTION_1080P_30	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080P", "OK 1080P30", "???"),
+	HDMI_RESOLUTION_1080P_50	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080P", "OK 1080P50", "???"),
+	HDMI_RESOLUTION_1080P_60	(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD 1080P", "OK 1080P60", "???"),
+	
+	HDMI_RESOLUTION_SRC			(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD SRC", "OK SRC", "???"),
+	HDMI_RESOLUTION_AUTO		(OppoBlurayPlayerCommandType.HDMI_RESOLUTION, StringType.EMPTY,  "QHD", "SHD AUTO", "OK AUTO", "???"),
+	
+	; // Semicolon for end of Enum declarations 
 	
 	final static String TO_PLAYER_PREFIX="#";		
 	final static String FROM_PLAYER_PREFIX="@";		
--- bundles/binding/org.openhab.binding.oppoblurayplayer/src/main/java/org/openhab/binding/oppoblurayplayer/internal/OppoBlurayPlayerUnknownCommandException.java
+++ bundles/binding/org.openhab.binding.oppoblurayplayer/src/main/java/org/openhab/binding/oppoblurayplayer/internal/OppoBlurayPlayerUnknownCommandException.java
@@ -1,5 +1,6 @@
 package org.openhab.binding.oppoblurayplayer.internal;
 
+
 public class OppoBlurayPlayerUnknownCommandException extends OppoBlurayPlayerException {
 
 	private static final long serialVersionUID = 1748479003112491205L;
